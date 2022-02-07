4a00266ec111784e0c0ed44a94221bf6038aac51
jiajun-c <2966623227@qq.com>
2022-02-07 19:24:28 +0800

    add

--- 2. 应用层/code/c1.py
+++ 2. 应用层/code/c1.py
@@ -1,41 +1,40 @@
-# import socket module
+#import socket module
 from audioop import add
 from multiprocessing import connection
 from socket import *
-from stat import filemode
-
+from stat import filemode 
 serverSocket = socket(AF_INET, SOCK_STREAM)
-# Prepare a sever socket
-# Fill in start
-serverSocket.bind(("", 83))
+#Prepare a sever socket
+#Fill in start
+serverSocket.bind(("",83))
 serverSocket.listen(1)
-# Fill in end
+#Fill in end
 while True:
-    # Establish the connection
-    print("Ready to serve...")
+#Establish the connection
+    print ("Ready to serve...")
     connectionSocket, addr = serverSocket.accept()
     try:
         message = connectionSocket.recv(2048)
         filename = message.split()[1]
-        f = open(filename[0:])
+        f = open(filename[0:]) 
         outputdata = f.read()
-        # Fill in start #Fill in end
-        # Send one HTTP header line into socket
-        # Fill in start
+        #Fill in start #Fill in end
+        #Send one HTTP header line into socket
+        #Fill in start
         outputdata = 'HTTP/1.1 200 OK\r\n connection: close \r\n\r\n' + outputdata
-        # Fill in end
-        # Send the content of the requested file to the client
-        for i in range(0, len(outputdata)):
+        #Fill in end 
+        #Send the content of the requested file to the client
+        for i in range(0, len(outputdata)): 
             connectionSocket.send(outputdata[i].encode())
         connectionSocket.close()
     except IOError:
-        # Send response message for file not found
-        # Fill in start
+    #Send response message for file not found
+    #Fill in start
         outputdata = 'HTTP/1.1 404 Not Found\r\n\r\n'
-        # Fill in end
-        # Close client socket
-        # Fill in start
+    #Fill in end
+    #Close client socket
+    #Fill in start
         connectionSocket.send(outputdata.encode())
         break
-    # Fill in end
+    #Fill in end 
 serverSocket.close()
