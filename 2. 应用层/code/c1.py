d131a8971ccfe12f50516ce1cfb20fb933cb3417
jiajun-c <2966623227@qq.com>
2022-02-07 16:32:41 +0800

    add

--- 2. 应用层/code/c1.py
+++ 2. 应用层/code/c1.py
@@ -1,34 +1,41 @@
-# 改为Python3格式
-# import socket module
+#import socket module
+from audioop import add
+from multiprocessing import connection
 from socket import *
-
+from stat import filemode 
 serverSocket = socket(AF_INET, SOCK_STREAM)
-# Prepare a sever socket
-serverPort = 81
-serverSocket.bind(("", serverPort))
+#Prepare a sever socket
+#Fill in start
+serverSocket.bind(("",83))
 serverSocket.listen(1)
+#Fill in end
 while True:
-    # Establish the connection
-    print('Ready to serve...')
+#Establish the connection
+    print ("Ready to serve...")
     connectionSocket, addr = serverSocket.accept()
     try:
-        message = connectionSocket.recv(1024)
+        message = connectionSocket.recv(2048)
         filename = message.split()[1]
-        f = open(filename[1:])
+        print (filename[0:]) 
+        f = open(filename[0:]) 
         outputdata = f.read()
-        f.close()
-        # Send one HTTP header line into socket
-        outputdata = 'HTTP/1.1 200 OK\r\n\r\n' + outputdata
-        # Send the content of the requested file to the client
-        for i in range(0, len(outputdata)):
+        #Fill in start #Fill in end
+        #Send one HTTP header line into socket
+        #Fill in start
+        outputdata = 'HTTP/1.1 200 OK\r\n connection: close \r\n\r\n' + outputdata
+        #Fill in end 
+        #Send the content of the requested file to the client
+        for i in range(0, len(outputdata)): 
             connectionSocket.send(outputdata[i].encode())
         connectionSocket.close()
-        print("OK!")
     except IOError:
-        # Send response message for file not found
+    #Send response message for file not found
+    #Fill in start
         outputdata = 'HTTP/1.1 404 Not Found\r\n\r\n'
-        # Close client socket
-        for i in range(0, len(outputdata)):
-            connectionSocket.send(outputdata[i].encode())
-        connectionSocket.close()
+    #Fill in end
+    #Close client socket
+    #Fill in start
+        connectionSocket.send(outputdata.encode())
+        break
+    #Fill in end 
 serverSocket.close()
---
+++ 2. 应用层/code/testc1.py
@@ -0,0 +1,16 @@
+from audioop import add
+from email import message
+from http import client
+from pydoc import cli
+from socket import *
+address = '127.0.0.1'
+port = 83
+client = socket(AF_INET,SOCK_STREAM)
+client.connect((address,port))
+message = "GET test.txt"
+while True:
+    client.send(message.encode())
+    reply = client.recv(2048).decode()
+    print(reply)
+    client.close()
+    break
